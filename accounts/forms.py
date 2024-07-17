645d07892fc3ffbc9a616756c8261e5d7c06d619
KristianRincon <krisdaringon@gmail.com>
2024-07-16 22:57:21 -0500

    Mejorando el aspecto de los formularios

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,11 +7,27 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'phone_number', 'address')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs = {'class': 'w-full p-2 border border-gray-300 rounded mt-1'}
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'address')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs = {'class': 'w-full p-2 border border-gray-300 rounded mt-1'}
 
 
 class CustomAuthenticationForm(AuthenticationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full p-2 border border-gray-300 rounded mt-1',
+                'placeholder': field.label
+            })
