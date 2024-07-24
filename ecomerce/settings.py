a306205f573cc98389086a5f57554b06edf2472f
KristianRincon <krisdaringon@gmail.com>
2024-07-23 19:59:00 -0500

    agregando imagen svg como carrito

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -119,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Ruta absoluta a la carpeta donde se almacenaran los archivos estaticos 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#Carpetas adicionales de archivos estaticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store/static'),
+]
 
 
 # Añadir configuraciones para archivos de medios
